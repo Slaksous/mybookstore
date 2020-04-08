91c909a3a3d4548379bb249b81d82cda805b8400
Lloyd <lloydjvrensburg@gmail.com>
2020-04-08 14:38:48 +0200

    Create Index New

--- routes/authors.js
+++ routes/authors.js
@@ -4,6 +4,7 @@ const Author = require('../models/author');
 
 // All Authors Route
 router.get('/', async (req, res) => {
+    
     let searchOptions = {};
     if(req.query.name != null && req.query.name !== '') {
         searchOptions.name = new RegExp(req.query.name, 'i');
